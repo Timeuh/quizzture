47b010db27c053c468fdd2da90e8db588b869daf
Timeuh <timeuuh@gmail.com>
2024-09-18 16:30:12 +0200

    ♻️ change main page text

--- app/page.tsx
+++ app/page.tsx
@@ -1,62 +1,30 @@
 import {css} from '@/styled-system/css';
-import Image from 'next/image';
+import {vstack} from '@/styled-system/patterns';
 
 export default function Home() {
   return (
-    <div>
-      <main>
-        <Image src='https://nextjs.org/icons/next.svg' alt='Next.js logo' width={180} height={38} priority />
-        <ol>
-          <li>
-            Get started by editing <code>app/page.tsx</code>.
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div>
-          <a
-            href='https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app'
-            target='_blank'
-            rel='noopener noreferrer'
-          >
-            <Image src='https://nextjs.org/icons/vercel.svg' alt='Vercel logomark' width={20} height={20} />
-            Deploy now
-          </a>
-          <a
-            href='https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app'
-            target='_blank'
-            rel='noopener noreferrer'
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer>
-        <a
-          href='https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app'
-          target='_blank'
-          rel='noopener noreferrer'
-        >
-          <Image aria-hidden src='https://nextjs.org/icons/file.svg' alt='File icon' width={16} height={16} />
-          Learn
-        </a>
-        <a
-          href='https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app'
-          target='_blank'
-          rel='noopener noreferrer'
-        >
-          <Image aria-hidden src='https://nextjs.org/icons/window.svg' alt='Window icon' width={16} height={16} />
-          Examples
-        </a>
-        <a
-          href='https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app'
-          target='_blank'
-          rel='noopener noreferrer'
-        >
-          <Image aria-hidden src='https://nextjs.org/icons/globe.svg' alt='Globe icon' width={16} height={16} />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+    <main
+      className={vstack({
+        gap: 4,
+        h: 'screen',
+        justifyContent: 'center',
+      })}
+    >
+      <h1
+        className={css({
+          fontSize: '6xl',
+          fontWeight: 'bold',
+        })}
+      >
+        QuizzTure
+      </h1>
+      <h2
+        className={css({
+          fontSize: '3xl',
+        })}
+      >
+        Mesurez votre culture et montrez qui est le plus fort !
+      </h2>
+    </main>
   );
 }
