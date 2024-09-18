202f065af0190b78ebfa1d02206b6c4597de0591
Timeuh <timeuuh@gmail.com>
2024-09-18 17:37:02 +0200

    🔧 install and config socket io

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,45 @@
+'use client';
+
+import {socket} from '@/socket';
 import {css} from '@/styled-system/css';
 import {vstack} from '@/styled-system/patterns';
+import {useEffect, useState} from 'react';
 
 export default function Home() {
+  const [isConnected, setIsConnected] = useState(false);
+  const [transport, setTransport] = useState('N/A');
+
+  useEffect(() => {
+    if (socket.connected) {
+      onConnect();
+    }
+
+    function onConnect() {
+      setIsConnected(true);
+      setTransport(socket.io.engine.transport.name);
+      console.log('Utilisateur connecté au WebSocket');
+
+      socket.io.engine.on('upgrade', (transport) => {
+        setTransport(transport.name);
+      });
+
+      socket.emit('user_connected', {message: 'Un nouvel utilisateur est connecté'});
+    }
+
+    function onDisconnect() {
+      setIsConnected(false);
+      setTransport('N/A');
+    }
+
+    socket.on('connect', onConnect);
+    socket.on('disconnect', onDisconnect);
+
+    return () => {
+      socket.off('connect', onConnect);
+      socket.off('disconnect', onDisconnect);
+    };
+  }, []);
+
   return (
     <main
       className={vstack({
