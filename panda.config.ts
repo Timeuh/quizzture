6cc394e25e85af97eda4211b61da3520f20406d1
Timeuh <timeuuh@gmail.com>
2024-09-19 15:27:58 +0200

    ♻️ adjust panda config to eslint and prettier rules

--- panda.config.ts
+++ panda.config.ts
@@ -1,11 +1,11 @@
-import { defineConfig } from "@pandacss/dev";
+import {defineConfig} from '@pandacss/dev';
 
 export default defineConfig({
   // Whether to use css reset
   preflight: true,
 
   // Where to look for your css declarations
-  include: ["./app/**/*.{js,jsx,ts,tsx}"],
+  include: ['./app/**/*.{js,jsx,ts,tsx}'],
 
   // Files to exclude
   exclude: [],
@@ -41,7 +41,7 @@ export default defineConfig({
       },
     },
   },
-  
+
   // The output directory for your css system
-  outdir: "styled-system",
+  outdir: 'styled-system',
 });
