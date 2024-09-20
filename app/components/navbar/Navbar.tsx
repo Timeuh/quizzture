ec4a16d52f70a36f2d4e0d46f6842c6f9abf7371
Timeuh <timeuuh@gmail.com>
2024-09-20 13:45:51 +0200

    ✨ add navbar content

--- app/components/navbar/Navbar.tsx
+++ app/components/navbar/Navbar.tsx
@@ -1,11 +1,33 @@
+import Link from 'next/link';
+import Image from 'next/image';
+import {
+  cNavbar_accountContainer,
+  cNavbar_createAccountButton,
+  cNavbar_logo,
+  cNavbar_navbar,
+  cNavbar_title,
+} from '@components/navbar/Navbar.styles';
+
 export default function Navbar() {
   return (
-    <nav>
-      <ul>
-        <li>Home</li>
-        <li>About</li>
-        <li>Contact</li>
-      </ul>
+    <nav className={cNavbar_navbar}>
+      <Link href={'/'} className={cNavbar_title}>
+        QuizzTure
+        <Image
+          src={'/images/logo.png'}
+          alt={'quizz logo'}
+          width={0}
+          height={0}
+          sizes={'100vw'}
+          className={cNavbar_logo}
+        />
+      </Link>
+      <div className={cNavbar_accountContainer}>
+        <Link href={'/login'}>se connecter</Link>
+        <Link href={'/register'} className={cNavbar_createAccountButton}>
+          créer un compte
+        </Link>
+      </div>
     </nav>
   );
 }
