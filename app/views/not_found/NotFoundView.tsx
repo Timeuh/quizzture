e21ad35b45e74273fa194d68062cccda87ffff36
Timeuh <timeuuh@gmail.com>
2024-09-20 15:30:50 +0200

    ✨ create not found page view

---
+++ app/views/not_found/NotFoundView.tsx
@@ -0,0 +1,27 @@
+import Link from 'next/link';
+import Image from 'next/image';
+import {
+  vNotFound_link,
+  vNotFound_logo,
+  vNotFound_notFound,
+  vNotFound_title,
+} from '@views/not_found/NotFoundView.styles';
+
+export default function NotFoundView() {
+  return (
+    <main id='not-found' className={vNotFound_notFound}>
+      <Image
+        src={'/images/logo.png'}
+        alt={'quizz logo'}
+        width={0}
+        height={0}
+        sizes={'100vw'}
+        className={vNotFound_logo}
+      />
+      <h1 className={vNotFound_title}>Page introuvable</h1>
+      <Link href={'/'} className={vNotFound_link}>
+        Retour à l&apos;accueil
+      </Link>
+    </main>
+  );
+}
