d614f82cbaa98efaa2a8f22fb02b1b914f19ab67
Timeuh <timeuuh@gmail.com>
2024-09-23 16:45:08 +0200

    ✨ create component for google auth registration

---
+++ app/register/components/google_register/GoogleRegister.tsx
@@ -0,0 +1,16 @@
+'use client';
+
+import {CredentialResponse, GoogleLogin} from '@react-oauth/google';
+
+export default function GoogleRegister() {
+  return (
+    <GoogleLogin
+      onSuccess={(credentialResponse: CredentialResponse) => {
+        console.log(credentialResponse);
+      }}
+      onError={() => {
+        console.log('Login Failed');
+      }}
+    />
+  );
+}
