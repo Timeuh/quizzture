6ac794231b3cdf9d3ad6408cde33d6f9a739c266
Timeuh <timeuuh@gmail.com>
2024-09-24 12:23:40 +0200

    ✨ create http response codes constants

---
+++ app/utils/constants/api.ts
@@ -0,0 +1,21 @@
+/* -------------------------------------------------------------------------- */
+/*                            HTTP Response codes                             */
+/* -------------------------------------------------------------------------- */
+
+// request successful
+export const HTTP_OK = 200;
+
+// resource created
+export const HTTP_CREATED = 201;
+
+// can't process request
+export const HTTP_BAD_REQUEST = 400;
+
+// resource not found
+export const HTTP_NOT_FOUND = 404;
+
+// schema validation failed
+export const HTTP_SCHEMA_ERROR = 422;
+
+// any server error
+export const HTTP_SERVER_ERROR = 500;
