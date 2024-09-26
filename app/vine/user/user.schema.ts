e06420947c67b977f591f3aab295906bc9ec75a5
Timeuh <timeuuh@gmail.com>
2024-09-26 11:10:38 +0200

    🦺 create validator and type for user

---
+++ app/vine/user/user.schema.ts
@@ -0,0 +1,30 @@
+import vine from '@vinejs/vine';
+import {Infer} from '@vinejs/vine/types';
+
+/* -------------------------------------------------------------------------- */
+/*                                  Schemas                                   */
+/* -------------------------------------------------------------------------- */
+
+const userSchema = vine.object({
+  email: vine.string().email(),
+  password: vine.string().nullable(),
+  highest_score: vine.number(),
+  victories: vine.number(),
+  victories_chain: vine.number(),
+  victories_three: vine.number(),
+  games: vine.number(),
+  games_chain: vine.number(),
+  games_three: vine.number(),
+});
+
+/* -------------------------------------------------------------------------- */
+/*                                   Types                                    */
+/* -------------------------------------------------------------------------- */
+
+export type User = Infer<typeof userSchema>;
+
+/* -------------------------------------------------------------------------- */
+/*                                 Validators                                 */
+/* -------------------------------------------------------------------------- */
+
+export const userValidator = vine.compile(userSchema);
