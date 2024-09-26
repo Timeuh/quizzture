8d060b1aaafe89dd0096639f7199fbc7db62bcab
Timeuh <timeuuh@gmail.com>
2024-09-26 16:11:30 +0200

    ♻️ adapt function to read any jwt

--- app/utils/functions/jwt/readJwt.ts
+++ app/utils/functions/jwt/readJwt.ts
@@ -1,23 +1,14 @@
-import {UserPayload} from '@utils/types/api';
-import {jwtVerify} from 'jose';
+import {decodeJwt} from 'jose';
 
 /**
  * Read a jwt
  *
  * @param {string} token : the token to decode
  */
-const readJwt = async (token: string): Promise<UserPayload | false> => {
-  // check env variable for jwt
-  if (!process.env.NEXT_PUBLIC_JWT_SECRET) {
-    throw new Error('You must define an env variable with the name NEXT_PUBLIC_JWT_SECRET');
-  }
-
-  // encode secret
-  const secret = new TextEncoder().encode(process.env.NEXT_PUBLIC_JWT_SECRET);
-
+const readJwt = async (token: string): Promise<unknown | false> => {
   // decode and return the jwt
   try {
-    return await jwtVerify(token, secret, {algorithms: ['HS256']});
+    return await decodeJwt(token);
   } catch (error: unknown) {
     console.log(error);
     return false;
