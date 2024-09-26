1fff43887c44e3d3232357c7f7c725a064bb1173
Timeuh <timeuuh@gmail.com>
2024-09-26 14:05:21 +0200

    ✨ add error handling to google login

--- app/register/components/google_register/GoogleRegister.tsx
+++ app/register/components/google_register/GoogleRegister.tsx
@@ -1,17 +1,24 @@
 'use client';
 
 import {CredentialResponse, GoogleLogin} from '@react-oauth/google';
+import {useState} from 'react';
+import {cGoogleRegister_error, cGoogleRegister_register} from './GoogleRegister.styles';
 
 export default function GoogleRegister() {
+  const [error, setError] = useState<string>('');
+
   return (
-    <GoogleLogin
-      onSuccess={(credentialResponse: CredentialResponse) => {
-        console.log(credentialResponse);
-      }}
-      onError={() => {
-        console.log('Login Failed');
-      }}
-      text='signup_with'
-    />
+    <section id='google_login' className={cGoogleRegister_register}>
+      <h3 className={cGoogleRegister_error}>{error}</h3>
+      <GoogleLogin
+        onSuccess={(credentialResponse: CredentialResponse) => {
+          console.log(credentialResponse);
+        }}
+        onError={() => {
+          setError('Connexion à Google échouée, veuillez rééssayer');
+        }}
+        text='signup_with'
+      />
+    </section>
   );
 }
