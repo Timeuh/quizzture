35a22ffead19be25a4c1569d6529216c842dc1d3
Timeuh <timeuuh@gmail.com>
2024-09-26 11:59:10 +0200

    ✨ create action to call api for create user

---
+++ app/actions/user/createUser.ts
@@ -0,0 +1,18 @@
+/**
+ * Call the api to create a new user
+ *
+ * @param {string} email : the email of the user
+ * @param {string} password : the password of the user
+ * @param {'email' | 'google'} type : the type of registration, via email or google auth
+ */
+const createUser = async (email: string, password: string, type: 'email' | 'google'): Promise<Response> => {
+  return await fetch(`${process.env.NEXT_PUBLIC_API_URL}/users`, {
+    method: 'POST',
+    body: JSON.stringify({email, password, type}),
+    cache: 'no-cache',
+  }).then((response: Response) => {
+    return response.json();
+  });
+};
+
+export default createUser;
