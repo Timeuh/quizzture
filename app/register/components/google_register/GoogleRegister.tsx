911e9a58d02881a15c6f987a62015d8a3ab4ac11
Timeuh <timeuuh@gmail.com>
2024-09-26 14:08:03 +0200

    ♻️ move register handling into dedicated functions

--- app/register/components/google_register/GoogleRegister.tsx
+++ app/register/components/google_register/GoogleRegister.tsx
@@ -7,18 +7,18 @@ import {cGoogleRegister_error, cGoogleRegister_register} from './GoogleRegister.
 export default function GoogleRegister() {
   const [error, setError] = useState<string>('');
 
+  const handleSuccess = (credentialResponse: CredentialResponse) => {
+    console.log(credentialResponse);
+  };
+
+  const handleError = () => {
+    setError('Connexion à Google échouée, veuillez rééssayer');
+  };
+
   return (
     <section id='google_login' className={cGoogleRegister_register}>
       <h3 className={cGoogleRegister_error}>{error}</h3>
-      <GoogleLogin
-        onSuccess={(credentialResponse: CredentialResponse) => {
-          console.log(credentialResponse);
-        }}
-        onError={() => {
-          setError('Connexion à Google échouée, veuillez rééssayer');
-        }}
-        text='signup_with'
-      />
+      <GoogleLogin onSuccess={handleSuccess} onError={handleError} text='signup_with' />
     </section>
   );
 }
