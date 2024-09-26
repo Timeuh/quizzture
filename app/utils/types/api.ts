e74f773f038de4a9a49f2193b00f70cbf18f4c3e
Timeuh <timeuuh@gmail.com>
2024-09-26 15:29:45 +0200

    🏷️ add type for jwt user payload

--- app/utils/types/api.ts
+++ app/utils/types/api.ts
@@ -1,8 +1,22 @@
+import {User} from '@schemas/user/user.schema';
+
 // error returned by api
 export interface ApiError {
   error: {
     code: number;
     message: string;
     details: unknown;
+  };
+}
+
+export interface UserPayload {
+  payload: {
+    user: User;
+    iat: number;
+    iss: string;
+    exp: number;
+  };
+  protectedHeader: {
+    alg: string;
   };
 }
