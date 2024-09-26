f4b979cda621e5cde15011fb5698b7700a1065de
Timeuh <timeuuh@gmail.com>
2024-09-26 13:31:25 +0200

    ♻️ rename svg properties

--- app/components/icons/Gamepad.tsx
+++ app/components/icons/Gamepad.tsx
@@ -11,9 +11,9 @@ export default function Gamepad({className}: Props) {
       viewBox='0 0 24 24'
       fill='none'
       stroke='currentColor'
-      stroke-width='2'
-      stroke-linecap='round'
-      stroke-linejoin='round'
+      strokeWidth='2'
+      strokeLinecap='round'
+      strokeLinejoin='round'
       className={className}
     >
       <line x1='6' x2='10' y1='11' y2='11' />
--- app/components/icons/Join.tsx
+++ app/components/icons/Join.tsx
@@ -11,9 +11,9 @@ export default function Join({className}: Props) {
       viewBox='0 0 24 24'
       fill='none'
       stroke='currentColor'
-      stroke-width='2'
-      stroke-linecap='round'
-      stroke-linejoin='round'
+      strokeWidth='2'
+      strokeLinecap='round'
+      strokeLinejoin='round'
       className={className}
     >
       <path d='M15 3h4a2 2 0 0 1 2 2v14a2 2 0 0 1-2 2h-4' />
