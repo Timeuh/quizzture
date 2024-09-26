20e08b32cc994150531a16afbddc6c997c37ed35
Timeuh <timeuuh@gmail.com>
2024-09-26 11:08:51 +0200

    ♻️ move types and validators into dedicated files

--- app/api/user/route.ts
+++ app/api/user/route.ts
@@ -1,32 +1,10 @@
+import {Credentials, credentialsValidator} from '@schemas/credentials/credentials.schema';
+import {User} from '@schemas/user/user.schema';
 import {HTTP_CREATED} from '@utils/constants/api';
 import sendErrorResponse from '@utils/functions/api/sendErrorResponse';
 import sendJsonResponse from '@utils/functions/api/sendJsonResponse';
 import encryptPassword from '@utils/functions/bcrypt/encryptPassword';
 import {prisma} from '@utils/prisma/client';
-import vine from '@vinejs/vine';
-import {Infer} from '@vinejs/vine/types';
-
-export const credentialsSchema = vine.object({
-  email: vine.string().email(),
-  type: vine.enum(['google', 'email']),
-  password: vine.string().optional().requiredWhen('type', '=', 'email'),
-});
-
-export const credentialsValidator = vine.compile(credentialsSchema);
-
-type Credentials = Infer<typeof credentialsSchema>;
-
-type User = {
-  email: string;
-  password: string | null;
-  highest_score: number;
-  victories: number;
-  victories_chain: number;
-  victories_three: number;
-  games: number;
-  games_chain: number;
-  games_three: number;
-};
 
 /**
  * Create a new user
