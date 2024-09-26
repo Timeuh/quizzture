ac1f54d998170957063e9b2552f03f1de575c37d
Timeuh <timeuuh@gmail.com>
2024-09-26 13:18:52 +0200

    📝 change return type

--- app/actions/user/createUser.ts
+++ app/actions/user/createUser.ts
@@ -1,3 +1,6 @@
+import {User} from '@schemas/user/user.schema';
+import {ApiError} from '@utils/types/api';
+
 /**
  * Call the api to create a new user
  *
@@ -5,7 +8,7 @@
  * @param {string} password : the password of the user
  * @param {'email' | 'google'} type : the type of registration, via email or google auth
  */
-const createUser = async (email: string, password: string, type: 'email' | 'google'): Promise<Response> => {
+const createUser = async (email: string, password: string, type: 'email' | 'google'): Promise<User | ApiError> => {
   return await fetch(`${process.env.NEXT_PUBLIC_API_URL}/users`, {
     method: 'POST',
     body: JSON.stringify({email, password, type}),
