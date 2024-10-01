439d8a0df637f2a48728684aeb436e3bf11dc668
Timeuh <timeuuh@gmail.com>
2024-10-01 14:16:01 +0200

    🏷️ add type for google auth jwt

--- app/utils/types/api.ts
+++ app/utils/types/api.ts
@@ -20,3 +20,21 @@ export interface UserPayload {
     alg: string;
   };
 }
+
+// auth from google auth
+export interface GoogleAuth {
+  iss: string;
+  azp: string;
+  aud: string;
+  sub: string;
+  email: string;
+  email_verified: boolean;
+  nbf: number;
+  name: string;
+  picture: string;
+  given_name: string;
+  family_name: string;
+  iat: number;
+  exp: number;
+  jti: string;
+}
