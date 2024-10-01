2936a5056cfc7355efbdef5384f76ff08a0901a4
Timeuh <timeuuh@gmail.com>
2024-10-01 17:58:04 +0200

    ✨ display user email in navbar if logged in

--- app/components/navbar/Navbar.tsx
+++ app/components/navbar/Navbar.tsx
@@ -1,3 +1,5 @@
+'use client';
+
 import Link from 'next/link';
 import Image from 'next/image';
 import {
@@ -8,8 +10,14 @@ import {
   cNavbar_navbar,
   cNavbar_title,
 } from '@components/navbar/Navbar.styles';
+import {useUserContext} from '@providers/UserProvider';
+import {UserPayload} from '@utils/types/api';
+import React from 'react';
 
 export default function Navbar() {
+  const user: UserPayload | null = useUserContext();
+  console.log(user);
+
   return (
     <nav className={cNavbar_navbar}>
       <Link href={'/'} className={cNavbar_title}>
@@ -24,12 +32,18 @@ export default function Navbar() {
         />
       </Link>
       <div className={cNavbar_accountContainer}>
-        <Link href={'/login'} className={cNavbar_loginButton}>
-          se connecter
-        </Link>
-        <Link href={'/register'} className={cNavbar_createAccountButton}>
-          créer un compte
-        </Link>
+        {user ? (
+          <h1>{user.payload.email}</h1>
+        ) : (
+          <React.Fragment>
+            <Link href={'/login'} className={cNavbar_loginButton}>
+              se connecter
+            </Link>
+            <Link href={'/register'} className={cNavbar_createAccountButton}>
+              créer un compte
+            </Link>
+          </React.Fragment>
+        )}
       </div>
     </nav>
   );
