fef1827311c09d5ea4900876d04a5215e3b963c5
Timeuh <timeuuh@gmail.com>
2024-10-01 14:16:25 +0200

    ✨ register google user and handle errors

--- app/register/components/google_register/GoogleRegister.tsx
+++ app/register/components/google_register/GoogleRegister.tsx
@@ -3,12 +3,37 @@
 import {CredentialResponse, GoogleLogin} from '@react-oauth/google';
 import {useState} from 'react';
 import {cGoogleRegister_error, cGoogleRegister_register} from './GoogleRegister.styles';
+import readJwt from '@utils/functions/jwt/readJwt';
+import {GoogleAuth} from '@utils/types/api';
+import createUser from '@actions/user/createUser';
+import {useRouter} from 'next/navigation';
 
 export default function GoogleRegister() {
   const [error, setError] = useState<string>('');
 
-  const handleSuccess = (credentialResponse: CredentialResponse) => {
-    console.log(credentialResponse);
+  const router = useRouter();
+
+  const handleSuccess = async (credentialResponse: CredentialResponse) => {
+    // decode google auth jwt
+    const decoded: GoogleAuth = (await readJwt(credentialResponse.credential || '')) as GoogleAuth;
+    // create the user
+    const creation = await createUser(decoded.email, '', 'google');
+
+    // if the creation failed
+    if ('error' in creation) {
+      // if the email is already used
+      if (creation.error.message === 'Duplicate error') {
+        setError('Ce compte est déjà utilisé');
+        return;
+      }
+
+      // otherwise
+      setError('Une erreur est survenue, veuillez réessayer');
+      return;
+    }
+
+    // redirect to the home page
+    router.push('/');
   };
 
   const handleError = () => {
