d96aff15fedd08e6792e1f0a0f96636640c9793f
Timeuh <timeuuh@gmail.com>
2024-10-03 11:48:40 +0200

    ✨ display nav menu when clicking user email

--- app/components/navbar/Navbar.tsx
+++ app/components/navbar/Navbar.tsx
@@ -12,38 +12,48 @@ import {
 } from '@components/navbar/Navbar.styles';
 import {useUserContext} from '@providers/UserProvider';
 import {UserPayload} from '@utils/types/api';
-import React from 'react';
+import React, {useState} from 'react';
+import NavMenu from '@components/nav_menu/NavMenu';
 
 export default function Navbar() {
+  const [showMenu, setShowMenu] = useState<boolean>(false);
+
   const user: UserPayload | null = useUserContext();
 
+  const toggleMenu = () => {
+    setShowMenu(!showMenu);
+  };
+
   return (
-    <nav className={cNavbar_navbar}>
-      <Link href={'/'} className={cNavbar_title}>
-        QuizzTure
-        <Image
-          src={'/images/logo.png'}
-          alt={'quizz logo'}
-          width={50}
-          height={50}
-          sizes={'100vw'}
-          className={cNavbar_logo}
-        />
-      </Link>
-      <div className={cNavbar_accountContainer}>
-        {user ? (
-          <h1>{user.payload.email}</h1>
-        ) : (
-          <React.Fragment>
-            <Link href={'/login'} className={cNavbar_loginButton}>
-              se connecter
-            </Link>
-            <Link href={'/register'} className={cNavbar_createAccountButton}>
-              créer un compte
-            </Link>
-          </React.Fragment>
-        )}
-      </div>
-    </nav>
+    <>
+      <nav className={cNavbar_navbar}>
+        <Link href={'/'} className={cNavbar_title}>
+          QuizzTure
+          <Image
+            src={'/images/logo.png'}
+            alt={'quizz logo'}
+            width={50}
+            height={50}
+            sizes={'100vw'}
+            className={cNavbar_logo}
+          />
+        </Link>
+        <div className={cNavbar_accountContainer}>
+          {user ? (
+            <button onClick={toggleMenu}>{user.payload.email}</button>
+          ) : (
+            <React.Fragment>
+              <Link href={'/login'} className={cNavbar_loginButton}>
+                se connecter
+              </Link>
+              <Link href={'/register'} className={cNavbar_createAccountButton}>
+                créer un compte
+              </Link>
+            </React.Fragment>
+          )}
+        </div>
+      </nav>
+      <NavMenu shouldDisplay={showMenu} hideSelf={toggleMenu} />
+    </>
   );
 }
