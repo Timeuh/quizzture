51b0ebf7534ff9ca1974994d1843ae7becff84db
Timeuh <timeuuh@gmail.com>
2024-10-03 13:42:04 +0200

    ♻️ rename user logout action

--- app/actions/user/userLogout.ts
+++ app/actions/user/userLogout.ts
@@ -6,15 +6,15 @@ import {redirect} from 'next/navigation';
 /**
  * User logout
  */
-const logout = async (): Promise<void> => {
+const userLogout = (): Promise<void> => {
   // check cookie env variable
   if (!process.env.NEXT_PUBLIC_JWT_COOKIE) {
     throw new Error('You must define an env variable with the name NEXT_PUBLIC_JWT_COOKIE');
   }
 
   // delete the jwt cookie and redirect to home page
-  await cookies().delete(process.env.NEXT_PUBLIC_JWT_COOKIE);
+  cookies().delete(process.env.NEXT_PUBLIC_JWT_COOKIE);
   redirect('/');
 };
 
-export default logout;
+export default userLogout;
