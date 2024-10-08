f17d5b83adbe57cee48e219a6e4ae8f8fc9ca6cd
Timeuh <timeuuh@gmail.com>
2024-10-08 15:04:56 +0200

    ✨ get the username parameter

--- app/actions/user/createUser.ts
+++ app/actions/user/createUser.ts
@@ -6,14 +6,20 @@ import {ApiError} from '@utils/types/api';
 /**
  * Call the api to create a new user
  *
+ * @param {string} username : the user name
  * @param {string} email : the email of the user
  * @param {string} password : the password of the user
  * @param {'email' | 'google'} type : the type of registration, via email or google auth
  */
-const createUser = async (email: string, password: string, type: 'email' | 'google'): Promise<User | ApiError> => {
+const createUser = async (
+  username: string,
+  email: string,
+  password: string,
+  type: 'email' | 'google',
+): Promise<User | ApiError> => {
   return await fetch(`${process.env.NEXT_PUBLIC_API_URL}/users`, {
     method: 'POST',
-    body: JSON.stringify({email, password, type}),
+    body: JSON.stringify({username, email, password, type}),
     cache: 'no-cache',
   }).then((response: Response) => {
     return response.json();
