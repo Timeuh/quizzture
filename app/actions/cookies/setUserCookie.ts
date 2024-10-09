0e00384be9f2f9c295ce7bd34e586ba9e9d17430
Timeuh <timeuuh@gmail.com>
2024-10-09 17:47:12 +0200

    📝 add doc to user actions

--- app/actions/cookies/setUserCookie.ts
+++ app/actions/cookies/setUserCookie.ts
@@ -8,10 +8,12 @@ import {cookies} from 'next/headers';
  * @param {string} token : the jwt of the user
  */
 const setUserCookie = (token: string): void => {
+  // check cookie env variable
   if (!process.env.NEXT_PUBLIC_JWT_COOKIE) {
     throw new Error('You must define an env variable with the name NEXT_PUBLIC_JWT_COOKIE');
   }
 
+  // set the jwt cookie in the navigator
   cookies().set(process.env.NEXT_PUBLIC_JWT_COOKIE, token);
   return;
 };
