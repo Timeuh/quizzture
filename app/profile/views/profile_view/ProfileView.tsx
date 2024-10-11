dfb9bf48ab20eb1703a912dabaa037af43e889f2
Timeuh <timeuuh@gmail.com>
2024-10-11 18:12:54 +0200

    ♻️ use scores array to display profile scores

--- app/profile/views/profile_view/ProfileView.tsx
+++ app/profile/views/profile_view/ProfileView.tsx
@@ -8,11 +8,22 @@ import {
   vProfile_scoreContainer,
   vProfile_username,
 } from './ProfileView.styles';
+import {ProfileScore} from '@utils/types/profile';
 
 /**
  * Main view for the profile page
  */
 export default function ProfileView() {
+  // scores to display
+  const scores: ProfileScore[] = [
+    {title: 'Victoires', score: 0},
+    {title: 'Parties', score: 0},
+    {title: 'Victoires en chaîne', score: 0},
+    {title: 'Parties en chaîne', score: 0},
+    {title: 'Victoires en 3 vies', score: 0},
+    {title: 'Parties en 3 vies', score: 0},
+  ];
+
   return (
     <main className={vProfile_profile}>
       <section className={vProfile_container}>
@@ -29,12 +40,9 @@ export default function ProfileView() {
         </div>
         <ScoreDisplay title={'Plus haut score'} score={0} isEven={true} showPoints />
         <section className={vProfile_scoreContainer}>
-          <ScoreDisplay title={'Victoires'} score={0} isEven={false} />
-          <ScoreDisplay title={'Parties'} score={0} isEven={false} />
-          <ScoreDisplay title={'Victoires en chaîne'} score={0} isEven={true} />
-          <ScoreDisplay title={'Parties en chaîne'} score={0} isEven={true} />
-          <ScoreDisplay title={'Victoires en 3 vies'} score={0} isEven={false} />
-          <ScoreDisplay title={'Parties en 3 vies'} score={0} isEven={false} />
+          {scores.map((score: ProfileScore, index: number) => {
+            return <ScoreDisplay key={index} title={score.title} score={score.score} isEven={index > 1 && index < 4} />;
+          })}
         </section>
       </section>
     </main>
