30cd12ea8b95a9490a7669c5ac00ee8b137f0557
Timeuh <timeuuh@gmail.com>
2024-10-17 16:15:16 +0200

    ♻️ change configuration handling from form to states

--- app/game/[id]/components/game_category/GameCategory.tsx
+++ app/game/[id]/components/game_category/GameCategory.tsx
@@ -1,8 +1,10 @@
-import {ReactNode, useState} from 'react';
+import {ReactNode} from 'react';
 import {cGameCategory_category} from './GameCategory.styles';
 
 type Props = {
   name: string;
+  value: boolean;
+  setValue: (value: boolean) => void;
   children: ReactNode;
   isFirst?: boolean;
   isLastFirstRow?: boolean;
@@ -15,25 +17,30 @@ type Props = {
  * @param {string} name : the category name
  * @param {ReactNode} children : the category icon
  */
-export default function GameCategory({name, children, isFirst, isFirstLastRow, isLastFirstRow: isLastOfRow}: Props) {
-  const [isActive, setIsActive] = useState<boolean>(true);
-
+export default function GameCategory({
+  name,
+  value,
+  setValue,
+  children,
+  isFirst,
+  isFirstLastRow,
+  isLastFirstRow: isLastOfRow,
+}: Props) {
   /**
    * Toggle category active state
    */
   const toggleActive = () => {
-    setIsActive(!isActive);
+    setValue(!value);
   };
 
   return (
     <button
       type='button'
-      className={cGameCategory_category(isFirst, isFirstLastRow, isLastOfRow, isActive)}
+      className={cGameCategory_category(isFirst, isFirstLastRow, isLastOfRow, value)}
       onClick={toggleActive}
     >
       {children}
       <h4>{name}</h4>
-      <input type='checkbox' checked={true} value={name} hidden />
     </button>
   );
 }
--- app/game/[id]/components/game_configuration/GameConfiguration.tsx
+++ app/game/[id]/components/game_configuration/GameConfiguration.tsx
@@ -1,8 +1,7 @@
 'use client';
 
-import Chain from '@components/icons/Chain';
 import Heart from '@components/icons/Heart';
-import {FormEvent, useState} from 'react';
+import {useState} from 'react';
 import {
   cGameConfiguration_buttonsContainer,
   cGameConfiguration_container,
@@ -35,9 +34,10 @@ import Music from '@components/icons/Music';
 import Clap from '@components/icons/Clap';
 import Popcorn from '@components/icons/Popcorn';
 import BookMarked from '@components/icons/BookMarked';
-import {Gamemode} from '@utils/types/game';
+import {GameParameters, Gamemode} from '@utils/types/game';
 import rules from '@texts/lobby/rules';
 import GameCode from '../game_code/GameCode';
+import Chain from '@components/icons/Chain';
 
 type Props = {
   gameId: string;
@@ -49,7 +49,24 @@ type Props = {
  * @param {string} gameId : current game unique id
  */
 export default function GameConfiguration({gameId}: Props) {
-  const [gamemode, setGamemode] = useState<Gamemode>('three');
+  const [configuration, setConfiguration] = useState<GameParameters>({
+    gameId,
+    gamemode: 'three',
+    geography: true,
+    sport: true,
+    history: true,
+    french: true,
+    physics: true,
+    animals: true,
+    anime: true,
+    manga: true,
+    celebrities: true,
+    english: true,
+    music: true,
+    cinema: true,
+    series: true,
+    litteracy: true,
+  });
 
   /**
    * Change current gamemode
@@ -57,21 +74,21 @@ export default function GameConfiguration({gameId}: Props) {
    * @param mode {'three' | 'chain'} : the new gamemode
    */
   const changeGamemode = (mode: Gamemode) => {
-    setGamemode(mode);
+    setConfiguration((prev: GameParameters) => {
+      return {...prev, gamemode: mode};
+    });
   };
 
   /**
    * Create game with current parameters
-   *
-   * @param {FormEvent<HTMLFormElement>} event : the form submission event
    */
-  const handleSubmit = (event: FormEvent<HTMLFormElement>) => {
-    event.preventDefault();
-    console.log('GameConfiguration form submitted');
+  const handleSubmit = () => {
+    console.log('submission');
+    console.log(configuration);
   };
 
   return (
-    <form onSubmit={handleSubmit} className={cGameConfiguration_container}>
+    <section className={cGameConfiguration_container}>
       <section className={cGameConfiguration_infosContainer}>
         <section className={cGameConfiguration_gameContainer}>
           <GameCode gameId={gameId} />
@@ -81,7 +98,7 @@ export default function GameConfiguration({gameId}: Props) {
               <button
                 type='button'
                 id='three-lifes'
-                className={cGameConfiguration_gameModeButton(gamemode === 'three')}
+                className={cGameConfiguration_gameModeButton(configuration.gamemode === 'three')}
                 onClick={() => {
                   return changeGamemode('three');
                 }}
@@ -96,7 +113,7 @@ export default function GameConfiguration({gameId}: Props) {
               <button
                 type='button'
                 id='chain'
-                className={cGameConfiguration_gameModeButton(gamemode === 'chain')}
+                className={cGameConfiguration_gameModeButton(configuration.gamemode === 'chain')}
                 onClick={() => {
                   return changeGamemode('chain');
                 }}
@@ -109,61 +126,177 @@ export default function GameConfiguration({gameId}: Props) {
                 </div>
               </button>
             </div>
-            <input type='text' value={gamemode} readOnly hidden />
+            <input type='text' value={configuration.gamemode} readOnly hidden />
           </section>
         </section>
         <section className={cGameConfiguration_rulesContainer}>
           <h2 className={cGameConfiguration_gameRulesTitle}>Règles</h2>
-          <p className={cGameConfiguration_gameRules}>{rules[gamemode]}</p>
+          <p className={cGameConfiguration_gameRules}>{rules[configuration.gamemode]}</p>
         </section>
       </section>
       <section className={cGameConfiguration_themesContainer}>
         <h2 className={cGameConfiguration_themesTitle}>Thèmes</h2>
         <section className={cGameConfiguration_themesList}>
-          <GameCategory name='Sport' isFirst>
+          <GameCategory
+            name='Sport'
+            value={configuration.sport}
+            setValue={() => {
+              setConfiguration((prev: GameParameters) => {
+                return {...prev, sport: !prev.sport};
+              });
+            }}
+            isFirst
+          >
             <Trophy className={cGameConfiguration_icon} />
           </GameCategory>
-          <GameCategory name='Géographie'>
+          <GameCategory
+            name='Géographie'
+            value={configuration.geography}
+            setValue={() => {
+              setConfiguration((prev: GameParameters) => {
+                return {...prev, geography: !prev.geography};
+              });
+            }}
+          >
             <Map className={cGameConfiguration_icon} />
           </GameCategory>
-          <GameCategory name='Histoire'>
+          <GameCategory
+            name='Histoire'
+            value={configuration.history}
+            setValue={() => {
+              setConfiguration((prev: GameParameters) => {
+                return {...prev, history: !prev.history};
+              });
+            }}
+          >
             <Crown className={cGameConfiguration_icon} />
           </GameCategory>
-          <GameCategory name='Français'>
+          <GameCategory
+            name='Français'
+            value={configuration.french}
+            setValue={() => {
+              setConfiguration((prev: GameParameters) => {
+                return {...prev, french: !prev.french};
+              });
+            }}
+          >
             <Languages className={cGameConfiguration_icon} />
           </GameCategory>
-          <GameCategory name='Physique-Chimie' isLastFirstRow>
+          <GameCategory
+            name='Physique-Chimie'
+            value={configuration.physics}
+            setValue={() => {
+              setConfiguration((prev: GameParameters) => {
+                return {...prev, physics: !prev.physics};
+              });
+            }}
+            isLastFirstRow
+          >
             <Flask className={cGameConfiguration_icon} />
           </GameCategory>
-          <GameCategory name='Animaux'>
+          <GameCategory
+            name='Animaux'
+            value={configuration.animals}
+            setValue={() => {
+              setConfiguration((prev: GameParameters) => {
+                return {...prev, animals: !prev.animals};
+              });
+            }}
+          >
             <Cat className={cGameConfiguration_icon} />
           </GameCategory>
-          <GameCategory name='Anime'>
+          <GameCategory
+            name='Anime'
+            value={configuration.anime}
+            setValue={() => {
+              setConfiguration((prev: GameParameters) => {
+                return {...prev, anime: !prev.anime};
+              });
+            }}
+          >
             <Mask className={cGameConfiguration_icon} />
           </GameCategory>
-          <GameCategory name='Manga'>
+          <GameCategory
+            name='Manga'
+            value={configuration.manga}
+            setValue={() => {
+              setConfiguration((prev: GameParameters) => {
+                return {...prev, manga: !prev.manga};
+              });
+            }}
+          >
             <BookA className={cGameConfiguration_icon} />
           </GameCategory>
-          <GameCategory name='Célébrités'>
+          <GameCategory
+            name='Célébrités'
+            value={configuration.celebrities}
+            setValue={() => {
+              setConfiguration((prev: GameParameters) => {
+                return {...prev, celebrities: !prev.celebrities};
+              });
+            }}
+          >
             <Star className={cGameConfiguration_icon} />
           </GameCategory>
-          <GameCategory name='Anglais'>
+          <GameCategory
+            name='Anglais'
+            value={configuration.english}
+            setValue={() => {
+              setConfiguration((prev: GameParameters) => {
+                return {...prev, english: !prev.english};
+              });
+            }}
+          >
             <Text className={cGameConfiguration_icon} />
           </GameCategory>
-          <GameCategory name='Musique' isFirstLastRow>
+          <GameCategory
+            name='Musique'
+            value={configuration.music}
+            setValue={() => {
+              setConfiguration((prev: GameParameters) => {
+                return {...prev, music: !prev.music};
+              });
+            }}
+            isFirstLastRow
+          >
             <Music className={cGameConfiguration_icon} />
           </GameCategory>
-          <GameCategory name='Cinéma'>
+          <GameCategory
+            name='Cinéma'
+            value={configuration.cinema}
+            setValue={() => {
+              setConfiguration((prev: GameParameters) => {
+                return {...prev, cinema: !prev.cinema};
+              });
+            }}
+          >
             <Clap className={cGameConfiguration_icon} />
           </GameCategory>
-          <GameCategory name='Séries'>
+          <GameCategory
+            name='Séries'
+            value={configuration.series}
+            setValue={() => {
+              setConfiguration((prev: GameParameters) => {
+                return {...prev, series: !prev.series};
+              });
+            }}
+          >
             <Popcorn className={cGameConfiguration_icon} />
           </GameCategory>
-          <GameCategory name='Littérature'>
+          <GameCategory
+            name='Littérature'
+            value={configuration.litteracy}
+            setValue={() => {
+              setConfiguration((prev: GameParameters) => {
+                return {...prev, litteracy: !prev.litteracy};
+              });
+            }}
+          >
             <BookMarked className={cGameConfiguration_icon} />
           </GameCategory>
         </section>
       </section>
-    </form>
+      <button onClick={handleSubmit}>Soumettre</button>
+    </section>
   );
 }
