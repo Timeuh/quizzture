495993c8e0619bd6f71174de3cf82f4e0d05c8b7
Timeuh <timeuuh@gmail.com>
2024-10-17 16:14:05 +0200

    🏷️ add game configuration type

--- app/utils/types/game.ts
+++ app/utils/types/game.ts
@@ -1,2 +1,22 @@
 // possible game modes
 export type Gamemode = 'three' | 'chain';
+
+// game configuration parameters
+export interface GameParameters {
+  gameId: string;
+  gamemode: Gamemode;
+  sport: boolean;
+  geography: boolean;
+  history: boolean;
+  french: boolean;
+  physics: boolean;
+  animals: boolean;
+  anime: boolean;
+  manga: boolean;
+  celebrities: boolean;
+  english: boolean;
+  music: boolean;
+  cinema: boolean;
+  series: boolean;
+  litteracy: boolean;
+}
