13c3fb0c1c9170ca59e870bcb381b6b5a3357f23
Timeuh <timeuuh@gmail.com>
2024-10-21 11:02:57 +0200

    ✨ display user info on user form

--- app/game/[id]/components/profile_selection/ProfileSelection.tsx
+++ app/game/[id]/components/profile_selection/ProfileSelection.tsx
@@ -1,3 +1,5 @@
+'use client';
+
 import Image from 'next/image';
 import {
   cProfileSelection_button,
@@ -5,22 +7,33 @@ import {
   cProfileSelection_image,
   cProfileSelection_input,
 } from './ProfileSelection.styles';
+import {User} from '@schemas/user/user.schema';
+import {useUserContext} from '@providers/UserProvider';
 
 /**
  * User profile selection
  */
 export default function ProfileSelection() {
+  // get the user from the context
+  const user: User | null | undefined = useUserContext();
+
   return (
     <form className={cProfileSelection_container}>
       <Image
-        src={'/images/picture/logo1.png'}
+        src={user ? user.picture : '/images/picture/logo1.png'}
         alt={'profile picture'}
         width={50}
         height={50}
         sizes={'100vw'}
         className={cProfileSelection_image}
       />
-      <input type='text' placeholder='Pseudo' className={cProfileSelection_input} />
+      <input
+        type='text'
+        placeholder='Pseudo'
+        defaultValue={user ? user.username : ''}
+        className={cProfileSelection_input}
+      />
+      <input type='text' value={user ? user.picture : '/images/picture/logo1.png'} readOnly hidden />
       <button type='submit' className={cProfileSelection_button}>
         Rejoindre
       </button>
