de964fa4b5a2662f9305d5c1c9455c2f349b5315
Timeuh <timeuuh@gmail.com>
2024-10-21 11:24:40 +0200

    ✨ get submission data

--- app/game/[id]/components/profile_selection/ProfileSelection.tsx
+++ app/game/[id]/components/profile_selection/ProfileSelection.tsx
@@ -9,6 +9,7 @@ import {
 } from './ProfileSelection.styles';
 import {User} from '@schemas/user/user.schema';
 import {useUserContext} from '@providers/UserProvider';
+import {FormEvent} from 'react';
 
 /**
  * User profile selection
@@ -17,8 +18,27 @@ export default function ProfileSelection() {
   // get the user from the context
   const user: User | null | undefined = useUserContext();
 
+  /**
+   * Handle user participation
+   *
+   * @param {FormEvent<HTMLFormElement>} event : the form submission event
+   */
+  const handleSubmit = (event: FormEvent<HTMLFormElement>) => {
+    // prevent default behavior
+    event.preventDefault();
+
+    // get form data
+    const target = event.target as HTMLFormElement;
+    const formData = new FormData(target);
+
+    const username = formData.get('username') as string;
+    const picture = formData.get('picture') as string;
+
+    console.log(username, picture);
+  };
+
   return (
-    <form className={cProfileSelection_container}>
+    <form className={cProfileSelection_container} onSubmit={handleSubmit}>
       <Image
         src={user ? user.picture : '/images/picture/logo1.png'}
         alt={'profile picture'}
@@ -28,12 +48,13 @@ export default function ProfileSelection() {
         className={cProfileSelection_image}
       />
       <input
+        name='username'
         type='text'
         placeholder='Pseudo'
         defaultValue={user ? user.username : ''}
         className={cProfileSelection_input}
       />
-      <input type='text' value={user ? user.picture : '/images/picture/logo1.png'} readOnly hidden />
+      <input name='picture' type='text' value={user ? user.picture : '/images/picture/logo1.png'} readOnly hidden />
       <button type='submit' className={cProfileSelection_button}>
         Rejoindre
       </button>
