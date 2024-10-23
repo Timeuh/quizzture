825a83209882e228ead17c551481fcffd0ff7429
Timeuh <timeuuh@gmail.com>
2024-10-23 11:31:48 +0200

    💄 style the player display component

---
+++ app/components/player_display/PlayerDisplay.styles.ts
@@ -0,0 +1,14 @@
+import {circle, hstack} from '@styles/patterns';
+
+export const cPlayerDisplay_display = hstack({
+  gap: 4,
+  bg: 'platinum.700',
+  borderRadius: 'md',
+  px: 2,
+  py: 4,
+  w: '15vw',
+});
+
+export const cPlayerDisplay_picture = circle({
+  size: 12,
+});
--- app/components/player_display/PlayerDisplay.tsx
+++ app/components/player_display/PlayerDisplay.tsx
@@ -1,3 +1,4 @@
+import {cPlayerDisplay_display, cPlayerDisplay_picture} from '@components/player_display/PlayerDisplay.styles';
 import {Player} from '@utils/types/game';
 import Image from 'next/image';
 
@@ -12,8 +13,15 @@ type Props = {
  */
 export default function PlayerDisplay({player}: Props) {
   return (
-    <div>
-      <Image src={player.picture} alt={'profile picture'} width={50} height={50} sizes={'100vw'} />
+    <div className={cPlayerDisplay_display}>
+      <Image
+        src={player.picture}
+        alt={'profile picture'}
+        width={50}
+        height={50}
+        sizes={'100vw'}
+        className={cPlayerDisplay_picture}
+      />
       <h3>{player.username}</h3>
     </div>
   );
