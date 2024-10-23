479f71ad503f4a6748334df633a804e35081d150
Timeuh <timeuuh@gmail.com>
2024-10-23 17:56:49 +0200

    🩹 return base games array when not in a game currently

--- app/utils/functions/sockets/handlePlayerListRequest.js
+++ app/utils/functions/sockets/handlePlayerListRequest.js
@@ -11,6 +11,11 @@ export const handlePlayerListRequest = (games, data, socket) => {
     return game.gameId === data.gameId;
   });
 
+  // if the game does not exist
+  if (!currentGame) {
+    return;
+  }
+
   // send all players in the game to the user
   socket.emit('receive_players', currentGame.players);
 };
