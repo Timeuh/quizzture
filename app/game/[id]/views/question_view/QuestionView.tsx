54e9948d17e0e47bd81e736bdb05edeab25ac4cb
Timeuh <timeuuh@gmail.com>
2024-11-08 16:07:20 +0100

    ✨ add 30s timer to question view and then change game state

--- app/game/[id]/views/question_view/QuestionView.tsx
+++ app/game/[id]/views/question_view/QuestionView.tsx
@@ -1,10 +1,38 @@
+import {useEffect, useState} from 'react';
+import {useGameContext} from '../../providers/GameProvider';
+
 /**
  * Display current question
  */
 export default function QuestionView() {
+  const [timeLeft, setTimeLeft] = useState<number>(30);
+
+  const {setGameState} = useGameContext();
+
+  useEffect(() => {
+    // if time left is 0, go to answer view
+    if (timeLeft === 0) {
+      setGameState('answer');
+      return;
+    }
+
+    // decrement time each second
+    const intervalId = setInterval(() => {
+      setTimeLeft((prevTime) => {
+        return prevTime - 1;
+      });
+    }, 1000);
+
+    // clear interval when component unmounts
+    return () => {
+      return clearInterval(intervalId);
+    };
+  }, [timeLeft, setGameState]);
+
   return (
     <section>
       <h1>QuestionView</h1>
+      <h2>{timeLeft} secondes restantes</h2>
     </section>
   );
 }
