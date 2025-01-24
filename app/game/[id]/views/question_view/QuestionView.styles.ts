df1ee79e2c1c657ec076501a8c9e715096deb5c6
Timeuh <timeuuh@gmail.com>
2025-01-24 12:31:17 +0100

    💄 adapt interface to question display

--- app/game/[id]/views/question_view/QuestionView.styles.ts
+++ app/game/[id]/views/question_view/QuestionView.styles.ts
@@ -3,14 +3,17 @@ import {hstack, vstack} from '@styles/patterns';
 
 export const vQuestionView_container = vstack({
   h: '3/4',
-  w: '100%',
+  w: '3/5',
+  gap: 12,
 });
 
 export const vQuestionView_header = hstack({
   justifyContent: 'space-between',
-  w: '3/5',
+  w: 'full',
 });
 
 export const vQuestionView_questionCount = css({
+  w: '15vw',
   fontSize: '3xl',
+  textAlign: 'right',
 });
