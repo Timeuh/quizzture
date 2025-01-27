98dcb2fca044f7d937eebfe9a7da5ff1c602fdb5
Timeuh <timeuuh@gmail.com>
2025-01-27 14:53:02 +0100

    ✨ create an array of icons for game categories

---
+++ app/utils/constants/icons.ts
@@ -0,0 +1,34 @@
+import BookA from '@components/icons/BookA';
+import BookMarked from '@components/icons/BookMarked';
+import Cat from '@components/icons/Cat';
+import Clap from '@components/icons/Clap';
+import Crown from '@components/icons/Crown';
+import Flask from '@components/icons/Flask';
+import Languages from '@components/icons/Languages';
+import Map from '@components/icons/Map';
+import Mask from '@components/icons/Mask';
+import Music from '@components/icons/Music';
+import Popcorn from '@components/icons/Popcorn';
+import Star from '@components/icons/Star';
+import Text from '@components/icons/Text';
+import Trophy from '@components/icons/Trophy';
+import {CategoryIcon} from '@utils/types/gameLobby';
+
+const iconsArray: CategoryIcon[] = [
+  {id: 1, Icon: Trophy},
+  {id: 2, Icon: Map},
+  {id: 3, Icon: Crown},
+  {id: 4, Icon: Languages},
+  {id: 5, Icon: Flask},
+  {id: 6, Icon: Cat},
+  {id: 7, Icon: Mask},
+  {id: 8, Icon: BookA},
+  {id: 9, Icon: Star},
+  {id: 10, Icon: Text},
+  {id: 11, Icon: Music},
+  {id: 12, Icon: Clap},
+  {id: 13, Icon: Popcorn},
+  {id: 14, Icon: BookMarked},
+];
+
+export default iconsArray;
